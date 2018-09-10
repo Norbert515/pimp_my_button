796993b084d7fefc456a8af62ce0d482c68e6574
Norbert <norbertkozsir@gmail.com>
2018-09-10 11:17:31 +0200

    Whops, forgot to dispose the AnimationController - fixed now :)

--- lib/src/pimp_my_button.dart
+++ lib/src/pimp_my_button.dart
@@ -43,6 +43,13 @@ class PimpedButtonState extends State<PimpedButton> with SingleTickerProviderSta
     });
   }
 
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
